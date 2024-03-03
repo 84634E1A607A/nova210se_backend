04d69c447d3bb936e0090cc52b52c5de169e9f42
ajax <ajax-dong@outlook.com>
2024-03-03 12:28:33 +0800

    feat: Added CORS & tested

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'main',
 ]
 
@@ -45,29 +44,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
@@ -114,13 +95,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN: str = "http://localhost:8000"
+
+# Apply local settings
+if os.path.exists(BASE_DIR / "settings.local.py"):
+    exec(open(BASE_DIR / "settings.local.py").read())
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.conf import settings
 
 
 def api(allowed_methods: list[str] = None):
@@ -12,8 +13,28 @@ def api(allowed_methods: list[str] = None):
     if allowed_methods is None:
         allowed_methods = ["GET"]
 
-    def decorator(function, *args, **kwargs):
-        def decorated(request, *args, **kwargs):
+    if "OPTIONS" not in allowed_methods:
+        allowed_methods.append("OPTIONS")
+
+    # Apply CORS headers
+    def with_cors(function):
+        def decorated(request, *args, **kwargs) -> HttpResponse:
+            response = function(request, *args, **kwargs)
+            response["Access-Control-Allow-Origin"] = settings.CORS_ORIGIN
+            response["Access-Control-Allow-Methods"] = ", ".join(allowed_methods)
+            return response
+
+        return decorated
+
+    def decorator(function):
+        @with_cors
+        def decorated(request, *args, **kwargs) -> HttpResponse:
+            # Always allow OPTIONS requests
+            if request.method == "OPTIONS":
+                response = HttpResponse()
+                response["Allow"] = ", ".join(allowed_methods)
+                return response
+
             # Check for allowed methods
             if request.method not in allowed_methods:
                 return JsonResponse(status=405, data={
@@ -21,13 +42,11 @@ def api(allowed_methods: list[str] = None):
                     "error": "Method not allowed"
                 })
 
-            if request.method == "OPTIONS":
-                response = HttpResponse()
-                response["Allow"] = ", ".join(allowed_methods)
-                return response
-
             try:
-                return JsonResponse(function(request, *args, **kwargs))
+                return JsonResponse({
+                    "ok": True,
+                    "data": function(request, *args, **kwargs),
+                })
             except Exception as e:
                 return JsonResponse(status=500, data={
                     "ok": False,
@@ -38,19 +57,7 @@ def api(allowed_methods: list[str] = None):
 
     return decorator
 
-# def api(function):
-#     """
-#     Decorator for all API views, checks for allowed methods, handles OPTIONS requests.
-#     """
-#     def decorated(request, *args, **kwargs):
-#         print(args, kwargs)
-#         return function(request, *args, **kwargs)
-#     return decorated
-
 
 @api(allowed_methods=["GET", "POST"])
 def login(request):
-    return {
-        "ok": True,
-        "message": "Hello, world!"
-    }
+    return "Login"
