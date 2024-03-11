e9007cad6ffb9aa9b822995689dede3a3d5a264d
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2024-03-11 13:44:50 +0800

    feat: Friend, friend group, friend invitation DB (#7)
    * feat: Added friend related DB
    
    * migrations

--- backend/main/models.py
+++ backend/main/models.py
@@ -10,3 +10,42 @@ class User(models.Model):
     auth_user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
     id = models.AutoField(primary_key=True)
     avatar_url = models.CharField(max_length=500)
+
+    # Allow this to be null so that there won't be an infinite loop when creating user
+    default_group = models.ForeignKey("FriendGroup", on_delete=models.CASCADE,
+                                      related_name="user_default_group", null=True)
+
+
+class FriendGroup(models.Model):
+    """
+    FriendGroup Model, stores friend groups of a user
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_group_user")
+    id = models.AutoField(primary_key=True)
+    default = models.BooleanField()
+    name = models.CharField(max_length=100)
+
+
+class Friend(models.Model):
+    """
+    Friend Model, stores friend relationship
+    """
+
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_user")
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_friend")
+    nickname = models.CharField(max_length=100)
+    group = models.ForeignKey(FriendGroup, on_delete=models.CASCADE, related_name="friend_friend_group")
+
+
+class FriendInvitation(models.Model):
+    """
+    FriendInvitation Model, stores friend invitations
+    """
+
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_invitation_user")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_invitation_friend")
+    comment = models.CharField(max_length=500)
+    source = models.IntegerField()
