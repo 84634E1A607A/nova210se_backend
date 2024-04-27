7f3b60bacbafb15182eac3bd91e0a6ca1b2dc892
ajax <ajax-dong@outlook.com>
2024-04-27 13:45:22 +0800

    fix: CD exception

--- backend/asgi.py
+++ backend/asgi.py
@@ -7,19 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
+# First we must set the DJANGO_SETTINGS_MODULE environment variable as soon as possible
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-from main.urls import ws_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-django_asgi_app = get_asgi_application()
+# Then we must setup django before importing models
+import django                                                           # noqa
+django.setup()
+
+# Now we can import the necessary modules
+from channels.auth import AuthMiddlewareStack                           # noqa
+from channels.routing import ProtocolTypeRouter, URLRouter              # noqa
+from channels.security.websocket import AllowedHostsOriginValidator     # noqa
+from django.core.asgi import get_asgi_application                       # noqa
+from main.urls import ws_urlpatterns                                    # noqa
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(ws_urlpatterns))),
 })
